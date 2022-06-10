7cfc5fda900978674612b765039863879b5a768a
Brian Lavery <brian.lavery@yahoo.co.uk>
2022-06-10 18:54:32 +0700

    set up maps initially

--- 03-jobs/src/screens/MapScreen.js
+++ 03-jobs/src/screens/MapScreen.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import MapView from 'react-native-maps';
 
 const MapScreen = () => {
 	return (
 		<SafeAreaView style={styles.container}>
-			<Text style={{ fontSize: 36 }}>MapScreen</Text>
+			<MapView
+				style={styles.map}
+				initialRegion={{
+					latitude: 13.728435051859396,
+					longitude: 100.5710083251757,
+					latitudeDelta: 0.03,
+					longitudeDelta: 0.03,
+				}}
+			/>
 		</SafeAreaView>
 	);
 };
@@ -18,8 +27,9 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
+	},
+	map: {
+		flex: 1,
 	},
 });
 
